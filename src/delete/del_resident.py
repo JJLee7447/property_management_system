7b2fc00a12dec0e63cbe9a7833d6038c8e916b7f
ljjjjlljj <1690523339@qq.com>
2023-06-14 19:22:36 +0800

    update

--- src/delete/del_resident.py
+++ src/delete/del_resident.py
@@ -4,16 +4,32 @@ from UI.delete_ui.del_resident_ui import Ui_del_resident
 import sys
 
 
+def GetResidentId():
+    conn = db_connect()
+    cursor = conn.cursor()
+    try:
+        sql = '''SELECT resident_id FROM residents;'''
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
 class DelResident(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_del_resident()
         self.ui.setupUi(self)
+        self.ui.resident_id_comboBox.addItems([str(i[0]) for i in GetResidentId()])
+        self.ui.resident_id_comboBox.currentIndexChanged.connect(self.update_resident_name)
+
         self.ui.pushButton.clicked.connect(self.del_resident_clicked)
 
     def del_resident_clicked(self):
         print("del_resident_clicked")
-        resident_id = self.ui.lineEdit.text()
+        resident_id = self.ui.resident_id_comboBox.currentText()
         name = self.ui.lineEdit_2.text()
         if resident_id == '' or name == '':
             print('resident_id or name are empty')
@@ -29,8 +45,23 @@ class DelResident(QtWidgets.QWidget):
 
         except Exception as e:
             print(e)
-        print('del_resident_success')
+        else:
+            print('del_resident_success')
 
+
+    def update_resident_name(self):
+        resident_id = self.ui.resident_id_comboBox.currentText()
+        conn = db_connect()
+        cursor = conn.cursor()
+        try:
+            sql = '''SELECT owner_name FROM residents WHERE resident_id = %s;'''
+            cursor.execute(sql, resident_id)
+            result = cursor.fetchone()
+            self.ui.lineEdit_2.setText(result[0])
+        except Exception as e:
+            print(e)
+        finally:
+            conn.close()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- src/delete/del_staff.py
+++ src/delete/del_staff.py
@@ -3,16 +3,33 @@ from PyQt5 import QtWidgets
 from src.db_config import db_connect
 import sys
 
+def GetStaffId():
+    conn = db_connect()
+    cursor = conn.cursor()
+    try:
+        sql = '''SELECT property_id FROM property_staff;'''
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return result
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
+
 class DelStaff(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_del_staff()
         self.ui.setupUi(self)
+        self.ui.staff_id_comboBox.addItems([str(i[0]) for i in GetStaffId()])
+
+        self.ui.staff_id_comboBox.currentIndexChanged.connect(self.staff_id_changed)
         self.ui.pushButton.clicked.connect(self.delete_staff_clicked)
+        self.ui.pushButton.clicked.connect(self.update_staff_id)
 
     def delete_staff_clicked(self):
-        property_id = self.ui.staff_id_lineEdit.text()
-        staff_name = self.ui.staff_id_lineEdit.text()
+        property_id = self.ui.staff_id_comboBox.currentText()
+        staff_name = self.ui.staff_name_lineEdit.text()
         if property_id == '' or staff_name == '':
             print('property_id or staff_name are empty')
             return
@@ -20,16 +37,34 @@ class DelStaff(QtWidgets.QWidget):
         conn = db_connect()
         cursor = conn.cursor()
         try:
-            sql = '''DELETE FROM property_staff WHERE property_id = %s or staff_name = %s;'''
+            sql = '''DELETE FROM property_staff WHERE property_id = %s and staff_name = %s;'''
             cursor.execute(sql, values)
             conn.commit()
 
         except Exception as e:
             print(e)
 
-        finally:
+        else:
             conn.close()
             print('staff_delete_success')
+
+    def staff_id_changed(self):
+        property_id = self.ui.staff_id_comboBox.currentText()
+        conn = db_connect()
+        cursor = conn.cursor()
+        try:
+            sql = '''SELECT staff_name FROM property_staff WHERE property_id = %s;'''
+            cursor.execute(sql, property_id)
+            result = cursor.fetchone()
+            self.ui.staff_name_lineEdit.setText(result[0])
+        except Exception as e:
+            print(e)
+        finally:
+            conn.close()
+
+    def update_staff_id(self):
+        self.ui.staff_id_comboBox.clear()
+        self.ui.staff_id_comboBox.addItems([str(i[0]) for i in GetStaffId()])
 
 
 if __name__ == "__main__":
